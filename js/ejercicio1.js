d68d19c307cc557fa698336d87143a59f7323017
RPJunco <rodrigojunco023@gmail.com>
2024-06-13 21:03:32 -0300

    Se agrego codigo al archivo ejercicio1.js. En el archivo se declara una variable de tipo Array, luego se utiliza una funcion para recorrer el array y luego devolver una lista nueva con los elementos del original pero elevado al cuadrado.

--- js/ejercicio1.js
+++ js/ejercicio1.js
@@ -1,1 +1,27 @@
+// Escribe una función que reciba un array de números y retorne un nuevo array con cada número elevado al cuadrado. Utiliza un bucle for...of para iterar sobre los elementos del array.
+
+// Declaracion de variables
+
+let numerosLista = [];
+
+// Funcion para elevar cada numero del arreglo y devolverlo en una lista
+function elevarNumeros(numList) {
+    let numerosAlCuadrado = [];
+
+    for (numeros of numList){
+        numerosAlCuadrado.push(Math.pow(numeros, 2));
+    }
+
+    return numerosAlCuadrado
+
+}
+
+// Se muestran los resultados por consola
+
+// Pasando los numero por parametro directamente
+console.log(elevarNumeros([1,2,3]));
+
+// Utilizando la funcion push para agregar valores a una variable
+numerosLista.push(10, 2, 5);
+console.log(elevarNumeros(numerosLista));
 
